6dcc8b4d1096612cb0408f06f57af589684c5bf7
amorpro <amorpro3@gmail.com>
2021-08-15 00:29:10 +0300

    baseHost argument for bin/smoke_tests.php

--- bin/smoke_tests.php
+++ bin/smoke_tests.php
@@ -6,17 +6,14 @@
  * Time: 23:36
  */
 
-use SmokeTests\Http\Header;
-use SmokeTests\Http\Response;
-use SmokeTests\Plugins\Display\Detailed;
-
 $testsFilePath = $argv[1];
+$baseHost      = $argv[2];
 
 include_once '../vendor/autoload.php';
 
 $tests = (new \SmokeTests\Config\Json())
     ->setNext(new \SmokeTests\Config\Php())
-    ->load($testsFilePath, 'http://landing');
+    ->load($testsFilePath, $baseHost);
 
 foreach ($tests as $test) {
     $handler = SmokeTests\Handler::createFromConfig($test);
